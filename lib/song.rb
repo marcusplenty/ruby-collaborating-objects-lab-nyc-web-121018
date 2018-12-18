81290b349ff126dd48643be76794749e68462128
Marcus Plenty <marcusplenty@gmail.com>
2018-12-18 02:39:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,6 @@ class Song
   def self.new_by_filename(filename)
     song_arr = filename.split("-")
     song = Song.new(song_arr[1].strip)
-    song.artist = 
+    song.artist = song_arr[0].strip
   end
 end 
