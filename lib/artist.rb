f09e34f5342bef9296f814c67b1384537a729c70
Marcus Plenty <marcusplenty@gmail.com>
2018-12-18 02:17:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,15 @@
 class Artist
+  @@all = []
   attr_accessor :name
   def initialize(name)
     @name = name 
   end
   def add_song(song)
     
+  end 
+  
+  def save
+    @@all << self
   end 
   
 end
